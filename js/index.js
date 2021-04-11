b1b7e0689f7374ad55f410289abaf20f19fe35c0
Baher902 <Baheratef10@gmail.com>
2021-04-11 19:49:31 +0200

    bootstrap

---
+++ js/index.js
@@ -0,0 +1,13 @@
+window.addEventListener('load',()=>{
+
+  document.querySelector('.loader').classList.add('fade');  
+
+});
+setTimeout(()=>{
+
+    document.querySelector('.loader').remove();  
+
+
+}, 1000)
+
+
